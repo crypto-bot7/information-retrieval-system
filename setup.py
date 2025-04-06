34d4507ebff7a6f06562dfa6176e4562a9330e9e
piyush <piyushatghara@gmail.com>
2025-04-06 19:53:59 +0530

    added reqs

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+from setuptools.config.expand import find_packages
+
+setup(
+    name= 'Generative AI Project',
+    version= '0.0.0',
+    author= 'patghara',
+    author_email= 'piyushatghara@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
